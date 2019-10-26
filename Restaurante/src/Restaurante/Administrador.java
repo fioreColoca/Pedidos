58adb7c8ad30176672fe1b6037eb41f6771229dc
seredonato <serefeli@hotmail.com>
2019-10-25 21:03:51 -0300

    Create Administrador.java

---
+++ Restaurante/src/Restaurante/Administrador.java
@@ -0,0 +1,10 @@
+package Restaurante;
+
+public class Administrador extends Usuario {
+
+	public Administrador(Integer Id, String nombre, String apellido, String email, String password) {
+		super(Id, nombre, apellido, email, password);
+	}
+
+
+}
