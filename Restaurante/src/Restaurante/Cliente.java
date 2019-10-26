c2d5350506c9f1fc17935f1bffc9b3af5453a51b
seredonato <serefeli@hotmail.com>
2019-10-25 21:03:55 -0300

    Create Cliente.java

---
+++ Restaurante/src/Restaurante/Cliente.java
@@ -0,0 +1,9 @@
+package Restaurante;
+
+public class Cliente extends Administrador {
+
+	public Cliente(Integer Id, String nombre, String apellido, String email, String password) {
+		super(Id, nombre, apellido, email, password);
+	}
+
+}
