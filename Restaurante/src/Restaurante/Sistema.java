3e7a69be955913b2a409d18a90330e656b818b1b
fioreColoca <fiorecoloca99@gmail.com>
2019-10-30 14:24:01 -0300

    Update Sistema.java

--- Restaurante/src/Restaurante/Sistema.java
+++ Restaurante/src/Restaurante/Sistema.java
@@ -35,27 +35,38 @@ public class Sistema {
 		return false;
 	}
 	
-	public Boolean buscarUsuario(String nombre, String apellido) {
+	public Boolean buscarUsuario(String email) {
 		for (Usuario usuarios:usuarios) {
-			if (usuarios.getNombre().contentEquals(nombre) && usuarios.getApellido().contentEquals(apellido))
+			if (usuarios.getEmail().contentEquals(email))
 				return true;
 		} return false;
 	}
 	
 
-	public Boolean EliminarUsuario(Integer Id) {
+	public Boolean EliminarUsuario(String email,String contraseña) {
 		Iterator<Usuario> it = usuarios.iterator();
 
 		while (it.hasNext()) {// devuelve true si tiene otro elemento y entra hasta que sea false
 
 			Usuario usuario = it.next();
-			if (usuario.getId().equals(Id)) {
+			if (usuario.getEmail().equals(email) && usuario.getPassword().equals(contraseña)) {
 				usuarios.remove(usuario);
 				return true;
 			}
 		}
 		return false;
 
+	}
+	public Boolean validarPassword(Usuario usuario) {
+
+		if (usuario.getPassword().length() >= 8) {
+			for (int i = 0; i < usuario.getPassword().length(); i++) {
+				if (Character.isDigit(usuario.getPassword().charAt(i)) == true) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 	
 	LinkedList<Producto> productos = new LinkedList<Producto>();
