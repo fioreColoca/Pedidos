21af9b98cd6000b1246569cadcd966304d055785
fioreColoca <fiorecoloca99@gmail.com>
2019-11-01 17:05:06 -0300

    Update TestRestaurante.java

--- Restaurante/Test/Test/TestRestaurante.java
+++ Restaurante/Test/Test/TestRestaurante.java
@@ -3,8 +3,8 @@ package Test;
 import org.junit.Assert;
 import org.junit.Test;
 
-import Restaurante.Administrador2;
-import Restaurante.Cliente2;
+import Restaurante.Administrador;
+import Restaurante.Cliente;
 import Restaurante.Mozo;
 import Restaurante.Sistema;
 import Restaurante.Usuario;
@@ -13,8 +13,8 @@ public class TestRestaurante {
 	//1
 	@Test
 	public void registrarUsuarioValido() {
-		Usuario usuario1 = new Usuario("djaskd", "daskd", "hjaksdhjak.com", "123qwer4");
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Usuario usuario1 = new Usuario("Laura", "Martinez", "lauraMartinez@gmail.com", "123qwer4");
+		Sistema sistema = new Sistema("cafeteria");
 		Boolean valorEsperado = true;
 		Boolean valorObtenido = sistema.crearNuevoUsuario(usuario1);
 		Assert.assertEquals(valorEsperado, valorObtenido);
@@ -22,10 +22,10 @@ public class TestRestaurante {
 	//2
 	@Test
 	public void registrarUsuarioNoValido() {
-		Sistema sistema = new Sistema("Serena cafeteria");
-		Usuario usuario1 = new Usuario("djaskd", "daskd", "mm.com", "123qwer4");
+		Sistema sistema = new Sistema("cafeteria");
+		Usuario usuario1 = new Usuario("Laura", "Martinez", "lauraMartinez@gmail.com", "123qwer4");
 		sistema.crearNuevoUsuario(usuario1);
-		Usuario usuario2 = new Usuario("djaskd", "daskd", "mm.com", "123qwer4");
+		Usuario usuario2 = new Usuario("Laura", "Martinez", "lauraMartinez@gmail.com", "123qwer4");
 		Boolean valorEsperado = false;
 		Boolean valorObtenido = sistema.crearNuevoUsuario(usuario2);
 		Assert.assertEquals(valorEsperado, valorObtenido);
@@ -33,8 +33,8 @@ public class TestRestaurante {
 	//3
 	@Test 
 	public void registrarUsuarioNoValido2() {
-		Sistema sistema = new Sistema("Serena cafeteria");
-		Usuario usuario1 = new Usuario("djaskd", "daskd", "mm.com", "123qwer4");
+		Sistema sistema = new Sistema("cafeteria");
+		Usuario usuario1 = new Usuario("Laura", "Martinez", "lauraMartinez@gmail.com", "123qwer4");
 		sistema.crearNuevoUsuario(usuario1);
 		Boolean valorEsperado = false;
 		Boolean valorObtenido = sistema.crearNuevoUsuario(usuario1);
@@ -43,8 +43,8 @@ public class TestRestaurante {
 	//4
 	@Test
 	public void passwordAceptada() {
-		Usuario usuario1 = new Usuario("djaskd", "daskd", "hjaksdhjak.com", "123qwer4");
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Usuario usuario1 = new Usuario("Laura", "Martinez", "lauraMartinez@gmail.com", "123qwer4");
+		Sistema sistema = new Sistema("cafeteria");
 		Boolean valorEsperado = true;
 		Boolean valorObtenido = sistema.validarPassword(usuario1);
 		Assert.assertEquals(valorEsperado, valorObtenido);
@@ -53,8 +53,8 @@ public class TestRestaurante {
 	//5
 	@Test
 	public void passwordNegado() {
-		Usuario usuario1 = new Usuario("djaskd", "daskd", "hjaksdhjak.com", "123");
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Usuario usuario1 = new Usuario("Laura", "Martinez", "lauraMartinez@gmail.com", "123");
+		Sistema sistema = new Sistema("cafeteria");
 		Boolean valorEsperado = false;
 		Boolean valorObtenido = sistema.validarPassword(usuario1);
 		Assert.assertEquals(valorEsperado, valorObtenido);
@@ -62,30 +62,30 @@ public class TestRestaurante {
 	//6
 	@Test
 	public void loguearUsuarioValido() {
-		Usuario usuario1 = new Usuario("djaskd", "daskd", "hjaksdhjak.com", "123qwer4");
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Usuario usuario1 = new Usuario("Laura", "Martinez", "lauraMartinez@gmail.com", "123qwer4");
+		Sistema sistema = new Sistema("cafeteria");
 		sistema.crearNuevoUsuario(usuario1);
-		sistema.loguearUsuario("hjaksdhjak.com", "123qwer4");
+		sistema.loguearUsuario("lauraMartinez@gmail.com", "123qwer4");
 		Boolean valorEsperado = true;
-		Boolean valorObtenido = sistema.loguearUsuario("hjaksdhjak.com", "123qwer4");
-		Assert.assertEquals(valorEsperado, valorObtenido);
+		Boolean valorObtenido = sistema.loguearUsuario("lauraMartinez@gmail.com", "123qwer4");
+		Assert.assertEquals(valorEsperado, valorObtenido);  //cambiar para q se use una cuenta a la vez
 	}
 	//7
 	@Test
 	public void loguearUsuarioNoValido() {
-		Usuario usuario1 = new Usuario("djaskd", "daskd", "hjaksdhjak.com", "123qwer4");
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Usuario usuario1 = new Usuario("Laura", "Martinez", "lauraMartinez@gmail.com", "123qwer4");
+		Sistema sistema = new Sistema("cafeteria");
 		sistema.crearNuevoUsuario(usuario1);
 		sistema.crearNuevoUsuario(usuario1);
 		Boolean valorEsperado = false;
-		Boolean valorObtenido = sistema.loguearUsuario("hjaksdhjak.com", "1qwer4");
+		Boolean valorObtenido = sistema.loguearUsuario("laurtinez@gmail.com", "123qwer4");
 		Assert.assertEquals(valorEsperado, valorObtenido);
 	}
 	//8
 	@Test
 	public void buscarUsuario() {
 		Usuario usuario1 = new Usuario("Macarena", "Rodriguez", "macarena123@.com", "123qwer4");
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Sistema sistema = new Sistema("cafeteria");
 		sistema.crearNuevoUsuario(usuario1);
 		sistema.loguearUsuario("macarena123@.com", "123qwer4");
 		Boolean valorEsperado = true;
@@ -97,65 +97,65 @@ public class TestRestaurante {
 	@Test
 	public void buscarUsuarioNoEncontrado() {
 		Usuario usuario1 = new Usuario("Macarena", "Rodriguez", "macarena123@.com", "123qwer4");
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Sistema sistema = new Sistema("cafeteria");
 		sistema.crearNuevoUsuario(usuario1);
 		sistema.loguearUsuario("macarena123@.com", "123qwer4");
 		Boolean valorEsperado = false;
-		Boolean valorObtenido = sistema.buscarUsuario("asddsa@gmail.com");
+		Boolean valorObtenido = sistema.buscarUsuario("macarena@gmail.com");
 		Assert.assertEquals(valorEsperado, valorObtenido);
 	}
 	//10
 	@Test
 	public void eliminarUsuarioEncontrado() {
-		Usuario usuario1 = new Usuario("djaskd", "daskd", "hjaksdhjak.com", "123qwer4");
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Usuario usuario1 = new Usuario("Macarena", "Rodriguez", "macarena123@.com", "123qwer4");
+		Sistema sistema = new Sistema("cafeteria");
 		sistema.crearNuevoUsuario(usuario1);
-		sistema.loguearUsuario("hjaksdhjak.com", "123qwer4");
+		sistema.loguearUsuario("macarena123@.com", "123qwer4");
 		Boolean valorEsperado = true;
-		Boolean valorObtenido = sistema.EliminarUsuario("hjaksdhjak.com","123qwer4");
+		Boolean valorObtenido = sistema.EliminarUsuario("macarena123@.com", "123qwer4");
 		Assert.assertEquals(valorEsperado, valorObtenido);
 	}
 	//11
 	@Test
 	public void eliminarUsuarioNoEncontrado() {
-		Usuario usuario1 = new Usuario("djaskd", "daskd", "hjaksdhjak.com", "123qwer4");
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Usuario usuario1 = new Usuario("Macarena", "Rodriguez", "macarena123@.com", "123qwer4");
+		Sistema sistema = new Sistema("cafeteria");
 		sistema.crearNuevoUsuario(usuario1);
-		sistema.loguearUsuario("hjaksdhjak.com", "123qwer4");
+		sistema.loguearUsuario("macarena123@.com", "123qwer4");
 		Boolean valorEsperado = false;
-		Boolean valorObtenido = sistema.EliminarUsuario("hjaksdhjakf.com","123qwer4");
+		Boolean valorObtenido = sistema.EliminarUsuario("macarena@gmail.com","123qwer4");
 		Assert.assertEquals(valorEsperado, valorObtenido);
 	}
 	//12
 	@Test
 	public void verificarEliminacionDeUsuario() {
-		Usuario usuario1 = new Usuario("djaskd", "daskd", "hjaksdhjak.com", "123qwer4");
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Usuario usuario1 = new Usuario("Macarena", "Rodriguez", "macarena123@.com", "123qwer4");
+		Sistema sistema = new Sistema("cafeteria");
 		sistema.crearNuevoUsuario(usuario1);
-		sistema.loguearUsuario("hjaksdhjak.com", "123qwer4");
-		sistema.EliminarUsuario("hjaksdhjak.com", "123qwer4");
+		sistema.loguearUsuario( "macarena123@.com", "123qwer4");
+		sistema.EliminarUsuario("macarena123@.com", "123qwer4");
 		Boolean valorEsperado = false;
-		Boolean valorObtenido = sistema.buscarUsuario("hjaksdhjak.com");
+		Boolean valorObtenido = sistema.buscarUsuario("macarena123@.com");
 		Assert.assertEquals(valorEsperado, valorObtenido);
 	}
 	//13
 	@Test
 	public void verificarNoEliminacionDeUsuarioo() {
-		Usuario usuario1 = new Usuario("djaskd", "daskd", "hjaksdhjak12.com", "123qwer4");
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Usuario usuario1 = new Usuario("Macarena", "Rodriguez", "macarena123@.com", "123qwer4");
+		Sistema sistema = new Sistema("cafeteria");
 		sistema.crearNuevoUsuario(usuario1);
-		sistema.loguearUsuario("hjaksdhjak12.com", "123qwer4");
-		sistema.EliminarUsuario("hjaksdhjajfklsdk.com", "123qwer4");
+		sistema.loguearUsuario("macarena123@.com", "123qwer4");
+		sistema.EliminarUsuario("macarena@.com", "123qwer4");
 		Boolean valorEsperado = true;
-		Boolean valorObtenido = sistema.buscarUsuario("hjaksdhjak12.com");
+		Boolean valorObtenido = sistema.buscarUsuario("macarena123@.com");
 		Assert.assertEquals(valorEsperado, valorObtenido);
 
 	}
 	//14
 	@Test
 	public void crearCliente() {
-		Cliente2 cliente = new Cliente2("djaskd", "daskd", "hjaksdhjak.com", "123qwer4",4);
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Cliente cliente = new Cliente("Macarena", "Rodriguez", "macarena123@.com", "123qwer4",4);
+		Sistema sistema = new Sistema("cafeteria");
 		Boolean valorEsperado = true;
 		Boolean valorObtenido = sistema.crearNuevoUsuario(cliente);
 		Assert.assertEquals(valorEsperado, valorObtenido);
@@ -163,8 +163,8 @@ public class TestRestaurante {
 	//15
 	@Test
 	public void crearMozo() {
-		Mozo mozo = new Mozo("djaskd", "daskd", "hjaksdhjak.com", "123qwer4",true);
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Mozo mozo = new Mozo("Macarena", "Rodriguez", "macarena123@.com", "123qwer4",true);
+		Sistema sistema = new Sistema("cafeteria");
 		Boolean valorEsperado = true;
 		Boolean valorObtenido = sistema.crearNuevoUsuario(mozo);
 		Assert.assertEquals(valorEsperado, valorObtenido);
@@ -172,8 +172,8 @@ public class TestRestaurante {
 	//16
 	@Test
 	public void crearAdministrador() {
-		Administrador2 adm = new Administrador2("djaskd", "daskd", "hjaksdhjak.com", "123qwer4","Dueño");
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Administrador adm = new Administrador("Macarena", "Rodriguez", "macarena123@.com", "123qwer4","Dueño");
+		Sistema sistema = new Sistema("cafeteria");
 		Boolean valorEsperado = true;
 		Boolean valorObtenido = sistema.crearNuevoUsuario(adm);
 		Assert.assertEquals(valorEsperado, valorObtenido);
@@ -181,10 +181,10 @@ public class TestRestaurante {
 	//17
 	@Test
 	public void probarLlamarMozo() {
-		Cliente2 cliente = new Cliente2("djaskd", "daskd", "hjaksdhjak.com", "123qwer4",4);
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Cliente cliente = new Cliente("Macarena", "Rodriguez", "macarena123@.com", "123qwer4",4);
+		Sistema sistema = new Sistema("cafeteria");
 		sistema.crearNuevoUsuario(cliente);
-		Mozo mozo = new Mozo("djaskd", "daskd", "hjaksdhjak.com", "123qwer4",true);
+		Mozo mozo = new Mozo("Alberto", "Pereyra", "alberto123Mozo@gmail.com", "123qwer4",true);
 		sistema.crearNuevoUsuario(mozo);
 		mozo.llamarMozo(cliente);
 		Boolean valorEsperado = true;
@@ -194,10 +194,10 @@ public class TestRestaurante {
 	//18
 	@Test
 	public void cancelarLlamadoMozo() {
-		Cliente2 cliente = new Cliente2("djaskd", "daskd", "hjaksdhjak.com", "123qwer4",4);
-		Sistema sistema = new Sistema("Serena cafeteria");
+		Cliente cliente = new Cliente("Macarena", "Rodriguez", "macarena123@.com", "123qwer4",4);
+		Sistema sistema = new Sistema("cafeteria");
 		sistema.crearNuevoUsuario(cliente);
-		Mozo mozo = new Mozo("djaskd", "daskd", "hjaksdhjak.com", "123qwer4",true);
+		Mozo mozo = new Mozo("Alberto", "Pereyra", "alberto123Mozo@gmail.com", "123qwer4",true);
 		sistema.crearNuevoUsuario(mozo);
 		mozo.llamarMozo(cliente);
 		mozo.candelarLLamado(cliente);
