14282acaa14075a56da2857b0b48f24f6502cce6
seredonato <serefeli@hotmail.com>
2019-11-06 15:49:12 -0300

    Create Restaurante.java

---
+++ Restaurante/src/Restaurante/Restaurante.java
@@ -0,0 +1,18 @@
+package Restaurante;
+
+public class Restaurante {
+
+	private String nombreRestaurante;
+	
+	public Restaurante (String nombreRestaurante) {
+		this.nombreRestaurante = nombreRestaurante;
+	}
+
+	public String getNombreRestaurante() {
+		return nombreRestaurante;
+	}
+
+	public void setNombreRestaurante(String nombreRestaurante) {
+		this.nombreRestaurante = nombreRestaurante;
+	}
+}
