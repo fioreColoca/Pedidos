e0d1f61289bcf33f6593549ccd29bab6cc6597ff
seredonato <serefeli@hotmail.com>
2019-11-06 16:18:47 -0300

    Update Restaurante.java

--- Restaurante/src/Restaurante/Restaurante.java
+++ Restaurante/src/Restaurante/Restaurante.java
@@ -1,18 +1,59 @@
 package Restaurante;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Restaurante {
 
-	private String nombreRestaurante;
-	
-	public Restaurante (String nombreRestaurante) {
-		this.nombreRestaurante = nombreRestaurante;
+	private String nombre2;
+	private Integer cuit2;
+
+	public Restaurante(String nombre2, int cuit2) {
+		this.nombre2=nombre2;
+		this.cuit2=cuit2;
+	}
+	LinkedList<Producto> productos = new LinkedList<Producto>();
+
+	public Boolean agregarProductor(Producto P, Administrador A, Sistema S, String email, String password) {
+		if (S.loguearUsuario(email, password)) {
+			if (!productos.contains(P)) {
+				this.productos.add(P);
+				return true;
+			}
+			return false;
+		}
+		return false;
 	}
 
-	public String getNombreRestaurante() {
-		return nombreRestaurante;
+	public Boolean eliminarProducto(Integer IdProducto, Administrador a) {
+		Iterator<Producto> it = productos.iterator();
+
+		while (it.hasNext()) {
+			Producto producto = it.next();
+			if (producto.getIdProducto().equals(IdProducto)) {
+				productos.remove(producto);
+				return true;
+			}
+		}
+		return false;
+
 	}
 
-	public void setNombreRestaurante(String nombreRestaurante) {
-		this.nombreRestaurante = nombreRestaurante;
+	public Boolean buscarProducto(Integer id) {
+		for (Producto p : productos) {
+			if (p.getIdProducto().equals(id))
+				return true;
+		}
+		return false;
 	}
+
+	public Boolean modificarPrecio(Double nuevoPrecio, Integer id) {
+		for (Producto p : productos) {
+			if (p.getIdProducto().equals(id))
+				p.setPrecio(nuevoPrecio);
+			return true;
+		}
+		return false;
+	}
+
 }
