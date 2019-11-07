5d5d752dcd0e5090c1ecf291ca7e0a08a8b4fc30
fioreColoca <fiorecoloca99@gmail.com>
2019-11-07 20:00:52 -0300

    Update Producto.java

--- Restaurante/src/Restaurante2/Producto.java
+++ Restaurante/src/Restaurante2/Producto.java
@@ -1,6 +1,6 @@
 package Restaurante2;
 
-public class Producto {
+public class Producto implements Comparable<Producto>{
 	private Categoria categoria;
 	private String descripcion;
 	private Double precio;
@@ -43,6 +43,57 @@ public class Producto {
 
 	public void setPrecio(Integer id) {
 		this.idProducto = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [categoria=" + categoria + ", descripcion=" + descripcion + ", precio=" + precio
+				+ ", idProducto=" + idProducto + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((categoria == null) ? 0 : categoria.hashCode());
+		result = prime * result + ((descripcion == null) ? 0 : descripcion.hashCode());
+		result = prime * result + ((idProducto == null) ? 0 : idProducto.hashCode());
+		result = prime * result + ((precio == null) ? 0 : precio.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Producto other = (Producto) obj;
+		if (categoria != other.categoria)
+			return false;
+		if (descripcion == null) {
+			if (other.descripcion != null)
+				return false;
+		} else if (!descripcion.equals(other.descripcion))
+			return false;
+		if (idProducto == null) {
+			if (other.idProducto != null)
+				return false;
+		} else if (!idProducto.equals(other.idProducto))
+			return false;
+		if (precio == null) {
+			if (other.precio != null)
+				return false;
+		} else if (!precio.equals(other.precio))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(Producto Producto){
+		return this.idProducto.compareTo(Producto.getIdProducto());
 	}
 	
 }
