8ef33cc5d1ef9eb55dc50027b7a6ca3b4b1a11f6
fioreColoca <fiorecoloca99@gmail.com>
2019-11-07 16:54:45 -0300

    Create Cliente.java

---
+++ Restaurante/src/Restaurante2/Cliente.java
@@ -0,0 +1,12 @@
+package Restaurante2;
+
+
+public class Cliente extends Usuario {
+
+	public Cliente(Integer Id, String nombre, String apellido, String email, String password) {
+		super(Id, nombre, apellido, email, password);
+	}
+	
+
+
+}
