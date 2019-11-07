8371615afa9042662254a8d443df734a5ed385fd
fioreColoca <fiorecoloca99@gmail.com>
2019-11-07 16:54:51 -0300

    Create Producto.java

---
+++ Restaurante/src/Restaurante2/Producto.java
@@ -0,0 +1,48 @@
+package Restaurante2;
+
+public class Producto {
+	private Categoria categoria;
+	private String descripcion;
+	private Double precio;
+	private Integer idProducto;
+	
+	public Producto (Categoria categoria,String descripcion,Double precio,Integer idProducto) {
+		this.categoria=categoria;
+		this.descripcion=descripcion;
+		this.precio=precio;
+		this.idProducto=idProducto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setProducto(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+		
+	}
+	public Integer getIdProducto() {
+		return idProducto;
+	}
+
+	public void setPrecio(Integer id) {
+		this.idProducto = id;
+	}
+	
+}
