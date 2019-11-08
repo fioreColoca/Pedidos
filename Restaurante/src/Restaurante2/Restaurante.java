23b1221d01c49c4c8ed92581d84ac710f13491ed
fioreColoca <fiorecoloca99@gmail.com>
2019-11-08 20:07:39 -0300

    Update Restaurante.java

--- Restaurante/src/Restaurante2/Restaurante.java
+++ Restaurante/src/Restaurante2/Restaurante.java
@@ -1,31 +1,34 @@
 package Restaurante2;
 
 import java.util.LinkedList;
-import java.util.Set;
-import java.util.TreeSet;
 
 
 public class Restaurante {
 	public Restaurante() {
-		this.productosSet  = new TreeSet<>();
 	} // NO FUNCIONA
+
 	LinkedList<Producto> productos = new LinkedList<Producto>();
-	private Set<Producto> productosSet; // NO FUNCIONA 
-	
-	public Set<Producto> getProductosSet() {
-		return productosSet;
-	} //NO FUNCIONA
-	
-	
+
+	/*
+	 * public Set<Producto> getProductosSet() { return productosSet; } //NO FUNCIONA
+	 */
 
 	private String nombre2;
 	private Integer cuit2;
 
 	public Restaurante(String nombre2, int cuit2) {
-		this.nombre2=nombre2;
-		this.cuit2=cuit2;
+		this.nombre2 = nombre2;
+		this.cuit2 = cuit2;
 	}
-	
+
+	public Boolean mostrarCarta() {
+		for (Producto P : productos) {
+			if (productos == null) {
+				return false;
+			}
+			System.out.println(P.hashCode() +"-------"+ P.toString());
+		}
+		return false;
+
+	}// ESTA MAL LO TENGO Q CAMBIAR
 }
-
-
