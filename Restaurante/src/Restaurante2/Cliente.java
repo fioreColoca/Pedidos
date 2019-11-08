3679e815e45b9826476cd32520d310c593e23ed6
fioreColoca <fiorecoloca99@gmail.com>
2019-11-08 20:07:31 -0300

    Update Cliente.java

--- Restaurante/src/Restaurante2/Cliente.java
+++ Restaurante/src/Restaurante2/Cliente.java
@@ -1,13 +1,59 @@
 package Restaurante2;
 
+import java.util.LinkedList;
 
 public class Cliente extends Usuario {
 
 	public Cliente(Integer Id, String nombre, String apellido, String email, String password) {
 		super(Id, nombre, apellido, email, password);
 	}
-	
-	
 
+	LinkedList<Producto> PedidosHistorial = new LinkedList<Producto>();
+	LinkedList<Producto> Pedido = new LinkedList<Producto>();
+
+	public Boolean pedirProducto(Integer idProducto, Restaurante R) {
+		for (Producto p : R.productos) {
+			if (p.getIdProducto().equals(idProducto)) {
+				this.Pedido.add(p);
+				this.PedidosHistorial.add(p);
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
+
+	public Double pedirCuenta(Restaurante R) {
+		Double acumuladorPrecio = 0.0;
+		for (Producto p : Pedido) {
+			if (Pedido == null) {
+			} else {
+				acumuladorPrecio += p.getPrecio();
+				Pedido.remove(p);
+			}
+
+		}
+		return acumuladorPrecio;
+	}
+	
+	public Boolean eliminarPedido(Integer IdProducto) {
+		for (Producto P : Pedido) {
+			if (Pedido == null) {
+				return false;
+			}else {
+				Pedido.remove(P);
+				return true;
+			}
+		}return false;
+	}
+	//NI IDEA:C
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
