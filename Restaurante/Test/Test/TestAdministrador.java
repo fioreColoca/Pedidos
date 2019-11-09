74bf0b23e234d7068cc91bc751c2da726084f3cd
fioreColoca <fiorecoloca99@gmail.com>
2019-11-08 21:05:56 -0300

    Create TestAdministrador.java

---
+++ Restaurante/Test/Test/TestAdministrador.java
@@ -0,0 +1,39 @@
+package Test;
+
+import org.junit.Test;
+
+import Restaurante2.Administrador;
+import Restaurante2.Categoria;
+import Restaurante2.Cliente;
+import Restaurante2.Producto;
+import Restaurante2.Restaurante;
+import Restaurante2.Sistema;
+
+public class TestAdministrador {
+	@Test
+	public void eliminarPedido() {
+		Cliente c1 = new Cliente(123, "lucia", "martinez", "luciamartinez@hotmail.com", "123luciamartinez");
+		Sistema sistema = new Sistema("La Fragata");
+		sistema.crearNuevoUsuario(c1);
+		sistema.loguearUsuario("luciamartinez@hotmail.com", "123luciamartinez");
+		Administrador a1 = new Administrador(456, "Marcelo", "Gomez", "MarceloGomez123@gmail.com", "123123");
+		Producto p1 = new Producto(Categoria.PIZZAS, "Muzzarella", 500.0, 2);
+		Producto p2 = new Producto(Categoria.EMPANADAS, "pollo", 50.0, 4);
+		Producto p3 = new Producto(Categoria.VINOS, "Tinto", 500.0, 2);
+		Producto p4 = new Producto(Categoria.BEBIDAS, "Agua", 60.0, 6);
+		Restaurante r1 = new Restaurante("Cafeteria", 123456);
+		a1.agregarProductos(p1, r1);
+		a1.agregarProductos(p2, r1);
+		a1.agregarProductos(p3, r1);
+		a1.agregarProductos(p4, r1);
+		c1.pedirProducto(4, r1);
+		c1.pedirProducto(4, r1);
+		c1.pedirProducto(6, r1);
+		c1.eliminarPedido(4);
+		c1.mostrarPedido(); 
+		
+		// COMO LO PLANTEAMOS NOSOTRAS NO IRIA UNA CLASE PEDIDO :C
+		
+	}
+
+}
