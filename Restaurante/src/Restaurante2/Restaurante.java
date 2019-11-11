65e3cf02683825ca30da4f5243bdb6c41695d22d
fioreColoca <fiorecoloca99@gmail.com>
2019-11-10 21:08:55 -0300

    Update Restaurante.java

--- Restaurante/src/Restaurante2/Restaurante.java
+++ Restaurante/src/Restaurante2/Restaurante.java
@@ -1,13 +1,18 @@
 package Restaurante2;
 
-import java.util.LinkedList;
+/*import java.util.LinkedList;*/
+import java.util.TreeSet;
 
 
 public class Restaurante {
 	public Restaurante() {
 	} // NO FUNCIONA
-
-	LinkedList<Producto> productos = new LinkedList<Producto>();
+	/*LinkedList<Producto> productos = new LinkedList<Producto>();*/
+	
+	TreeSet <Producto> productos = new TreeSet <Producto>();
+	// NO HACE FALTA NINGUN METODO Q DISTINGA LOS NUMEROS DE IDENTIFICACION PARA Q HAYA REPETIDOS
+	//PORQ LO HACE EL MISMO TREESET
+	// ESTA ORDENADO PARA FACILITAR EL INGRESO DEL PRODUCTO YA QUE SE TIENE QUE INGRESAR ID
 
 	/*
 	 * public Set<Producto> getProductosSet() { return productosSet; } //NO FUNCIONA
@@ -20,15 +25,16 @@ public class Restaurante {
 		this.nombre2 = nombre2;
 		this.cuit2 = cuit2;
 	}
-
+	//MUESTRA TODA LA ARRAY 
 	public Boolean mostrarCarta() {
+		Integer contador=0;
 		for (Producto P : productos) {
 			if (productos == null) {
 				return false;
 			}
-			System.out.println(P.hashCode() +"-------"+ P.toString());
+			System.out.println(P.hashCode() + " -- " + P.toString());
 		}
 		return false;
 
-	}// ESTA MAL LO TENGO Q CAMBIAR
+	}
 }
