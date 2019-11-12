1b05bffcb47e8d28528fc878417a72f558898bf6
fioreColoca <fiorecoloca99@gmail.com>
2019-11-12 13:54:44 -0300

    Update TestRestaurante.java

--- Restaurante/Test/Test/TestRestaurante.java
+++ Restaurante/Test/Test/TestRestaurante.java
@@ -50,29 +50,6 @@ public class TestRestaurante {
 			Boolean valorEsperado=false;
 			Assert.assertEquals(valorEsperado, valorObtenido);
 		}
-		/*@Test
-		public void ingresarConSet() throws productoException {
-			Cliente c1 = new Cliente(123, "lucia", "martinez", "luciamartinez@hotmail.com", "123luciamartinez");
-			Sistema sistema = new Sistema("La Fragata");
-			sistema.crearNuevoUsuario(c1);
-			sistema.loguearUsuario("luciamartinez@hotmail.com", "123luciamartinez");
-			Administrador a1 = new Administrador(456, "Marcelo", "Gomez", "MarceloGomez123@gmail.com", "123123");
-			Producto p1 = new Producto(Categoria.PIZZAS, "Muzzarella", 500.0, 2);
-			Producto p2 = new Producto(Categoria.EMPANADAS, "pollo", 50.0, 4);
-			Producto p3 = new Producto(Categoria.VINOS, "Tinto", 500.0, 2);
-			Producto p4 = new Producto(Categoria.BEBIDAS, "Agua", 60.0, 6);
-			Restaurante r1 = new Restaurante("Cafeteria", 123456);
-			a1.agregarProductos(p1, r1);
-			a1.agregarProductos(p2, r1);
-			a1.agregarProductos(p3, r1);
-			a1.agregarProductos(p4, r1);
-			
-			Assert.assertEquals(4, r1.getProductosSet().size());
-			
-			for (Producto cli : r1.getProductosSet()) {
-				System.out.println(cli.hashCode() +"-------"+ cli.toString());
-			}
-		} // NO FUNCIONA */
 		@Test
 		public void eliminarProductos() {
 			Cliente c1 = new Cliente(123, "lucia", "martinez", "luciamartinez@hotmail.com", "123luciamartinez");
