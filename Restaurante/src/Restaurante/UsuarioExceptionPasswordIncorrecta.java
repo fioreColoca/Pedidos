e2b960649d0aa86bfa99ba16a208e591b1cc6c39
seredonato <serefeli@hotmail.com>
2019-11-14 14:57:49 -0300

    Update UsuarioExceptionPasswordIncorrecta.java

--- Restaurante/src/Restaurante/UsuarioExceptionPasswordIncorrecta.java
+++ Restaurante/src/Restaurante/UsuarioExceptionPasswordIncorrecta.java
@@ -1,7 +1,7 @@
 package Restaurante;
 
-public class UsuarioExceptionContraseñaIncorrecta extends Exception{
-	public UsuarioExceptionContraseñaIncorrecta() {
+public class UsuarioExceptionPasswordIncorrecta extends Exception{
+	public UsuarioExceptionPasswordIncorrecta() {
 		super("La contraseña debe cumplir con los requisitos: contener al menos 8 caracteres, el primer caracter sea un digito");
 	}
 
