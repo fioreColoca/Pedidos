350ce295df9e3e9c21989225618a765e81fce2ba
fioreColoca <fiorecoloca99@gmail.com>
2019-11-14 17:10:06 -0300

    Create AdministradorExceptionEliminarProducto.java

---
+++ Restaurante/src/Restaurante2/AdministradorExceptionEliminarProducto.java
@@ -0,0 +1,7 @@
+package Restaurante2;
+
+public class AdministradorExceptionEliminarProducto extends Exception {
+	public AdministradorExceptionEliminarProducto() {
+		super("No se pudo Eliminar el Producto");
+	}
+}
