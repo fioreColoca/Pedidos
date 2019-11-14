188f20ebb09082220357362ae8a1d29c7f826750
fioreColoca <fiorecoloca99@gmail.com>
2019-11-14 15:54:44 -0300

    Create UsuarioExceptionPasswordIncorrecta.java

---
+++ Restaurante/src/Restaurante2/UsuarioExceptionPasswordIncorrecta.java
@@ -0,0 +1,8 @@
+package Restaurante2;
+
+public class UsuarioExceptionPasswordIncorrecta extends Exception{
+	public UsuarioExceptionPasswordIncorrecta() {
+		super("La contraseña debe cumplir con los requisitos: contener al menos 8 caracteres, el primer caracter sea un digito");
+	}
+
+}
