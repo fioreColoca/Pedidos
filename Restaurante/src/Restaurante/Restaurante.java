b9d9c6037ed210422d052a9f3ce3ed588becfe63
seredonato <serefeli@hotmail.com>
2019-11-14 17:31:45 -0300

    Update Restaurante.java

--- Restaurante/src/Restaurante/Restaurante.java
+++ Restaurante/src/Restaurante/Restaurante.java
@@ -2,63 +2,36 @@ package Restaurante;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.TreeSet;
 
 public class Restaurante {
+
+	TreeSet<Producto> productos = new TreeSet<Producto>();
+	TreeSet<Producto> productosPedidos = new TreeSet<Producto>();
+	// NO HACE FALTA NINGUN METODO Q DISTINGA LOS NUMEROS DE IDENTIFICACION PARA Q
+	// HAYA REPETIDOS
+	// PORQ LO HACE EL MISMO TREESET
+	// ESTA ORDENADO PARA FACILITAR EL INGRESO DEL PRODUCTO YA QUE SE TIENE QUE
+	// INGRESAR ID
 
 	private String nombre2;
 	private Integer cuit2;
 
 	public Restaurante(String nombre2, int cuit2) {
-		this.nombre2=nombre2;
-		this.cuit2=cuit2;
+		this.nombre2 = nombre2;
+		this.cuit2 = cuit2;
 	}
-	LinkedList<Producto> productos = new LinkedList<Producto>();
 
-	public Boolean agregarProductor(Producto P, Administrador A, Sistema S, String email, String password) throws SistemaExceptionNoEncuentraUsuario {
-		if (S.loguearUsuario(email, password)) {
-			if (!productos.contains(P)) {
-				this.productos.add(P);
-				return true;
+	// MUESTRA TODA LA ARRAY
+	public Boolean mostrarCarta() {
+		Integer contador = 0;
+		for (Producto P : productos) {
+			if (productos == null) {
+				return false;
 			}
-			return false;
-		}
-		return false;
-	}
-
-	public Boolean eliminarProducto(Integer IdProducto, Administrador a) {
-		Iterator<Producto> it = productos.iterator();
-
-		while (it.hasNext()) {
-			Producto producto = it.next();
-			if (producto.getIdProducto().equals(IdProducto)) {
-				productos.remove(producto);
-				return true;
-			}
+			System.out.println(P.hashCode() + " -- " + P.toString());
 		}
 		return false;
 
 	}
-
-	public Boolean buscarProducto(Integer id) {
-		for (Producto p : productos) {
-			if (p.getIdProducto().equals(id))
-				return true;
-		}
-		return false;
-	}
-
-	public Boolean modificarPrecio(Double nuevoPrecio, Integer id) {
-		for (Producto p : productos) {
-			if (p.getIdProducto().equals(id))
-				p.setPrecio(nuevoPrecio);
-			return true;
-		}
-		return false;
-	}
-	
-	public int mostrarCarta() { //NI IDEA SI ESTA BIEN
-			return productos.size();
-		}
-	}
-
-
+}
