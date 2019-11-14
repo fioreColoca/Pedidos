b2b29cfcc8bf6def4270e2abf171e009b85e5e4e
fioreColoca <fiorecoloca99@gmail.com>
2019-11-14 15:53:53 -0300

    Update Sistema.java

--- Restaurante/src/Restaurante2/Sistema.java
+++ Restaurante/src/Restaurante2/Sistema.java
@@ -6,101 +6,114 @@ import java.util.LinkedList;
 
 public class Sistema {
 	
-	public static Sistema instance ;
-	
-	Usuario usuarioLogueado;
-	
-	public Sistema() {}
+		public static Sistema instance;
 
-	public static Sistema getInstance() {
-		if (instance  == null) {
-			instance  = new Sistema();
+		Usuario usuarioLogueado;
+
+		public Sistema() {
 		}
-		
-		return instance;
-	}
-	LinkedList<Usuario> usuarios = new LinkedList<Usuario>();
-	LinkedList<Usuario> usuariosLogueados = new LinkedList<Usuario>();
-	
-//REGISTRAR
-	public Boolean crearNuevoUsuario(Usuario usuario) {
-		if (!usuarios.contains(usuario)) { // Se Asegura que no este la cuenta agregada anteriormente..
-			for (Usuario usuarios : usuarios) { // NombreClase,NombreOBJ:NombreList
-				if (usuarios.getEmail().equals(usuario.getEmail()))
-					 return false;	}
-			this.usuarios.add(usuario);
-			return true;
-		} return false;
-		 
-	}
 
-//ABRIR SESION
-	
-	public Boolean loguearUsuario(String email, String password) {
-		for (int i = 0; i < usuarios.size(); i++) {
-			if (usuarios.get(i).getEmail().equals(email) && usuarios.get(i).getPassword().equals(password)) {
-				Usuario usuario = usuarios.get(i);
-				this.usuariosLogueados.add(usuario);
-				usuarioLogueado = usuario;
-				return true;
+		public static Sistema getInstance() {
+			if (instance == null) {
+				instance = new Sistema();
 			}
-		} return false;
-	}
 
-//CERRAR SESION
-	public Boolean cerrarSesiondeUsuario() {
-		Iterator<Usuario> it = usuariosLogueados.iterator();
+			return instance;
+		}
 
-		while (it.hasNext()) {
+		LinkedList<Usuario> usuarios = new LinkedList<Usuario>();
+		LinkedList<Usuario> usuariosLogueados = new LinkedList<Usuario>();
 
-			Usuario usuarioAuxiliar = it.next();
-			if (usuarioAuxiliar.equals(usuarioLogueado)) {
-				usuariosLogueados.remove(usuarioLogueado);
+		// REGISTRAR
+		public Boolean crearNuevoUsuario(Usuario usuario) throws SistemaExceptionNoCreaUsuario {
+			if (!usuarios.contains(usuario)) { // Se Asegura que no este la cuenta agregada anteriormente..
+				for (Usuario usuarios : usuarios) {
+					if (usuarios.getEmail().equals(usuario.getEmail()))
+						throw new SistemaExceptionNoCreaUsuario();
+				}
+				this.usuarios.add(usuario);
 				return true;
+			} else
+				throw new SistemaExceptionNoCreaUsuario();
+
+		}
+
+		// ABRIR SESION
+		public Boolean loguearUsuario(String email, String password) throws SistemaExceptionNoEncuentraUsuario {
+			for (int i = 0; i < usuarios.size(); i++) {
+				if (usuarios.get(i).getEmail().equals(email) && usuarios.get(i).getPassword().equals(password)) {
+					Usuario usuario = usuarios.get(i);
+					this.usuariosLogueados.add(usuario);
+					return true;
+				}
 			}
+			throw new SistemaExceptionNoEncuentraUsuario();
 		}
-		return false;
 
-	}
-//BUSCAR USUARIOS ONLINE
-	public Boolean buscarUsuarioLogueado(Integer Id,Usuario usuarioBuscado) {
-		for (Usuario  usuarioLogueado  : usuariosLogueados) {
-		for (Usuario usuarios : usuariosLogueados) {
-			
-			if (usuarios.getId().equals(Id))
-				return true;
-		}
-		return false;
-		}return false;
-	}
-	
-//BUSCAR USUARIOS ONLINE/OFFLINE
-	public Boolean buscarUsuario(Integer Id,Usuario usuarioIngresado) {
-		for (Usuario  usuarioLogueado  : usuariosLogueados) {
-		for (Usuario  usuarioIngresado1  : usuariosLogueados) {
-			if  (usuarioIngresado1.equals(usuarioIngresado)) {
-		for (Usuario usuarios : usuarios) {
-			if (usuarios.getId().equals(Id))
-				return true;
-		}
-				return false;
-			} return false;
-		}return  false;
-		} return false;
-	}
-//ELIMINAR USUARIO 
+		// CERRAR SESION
+		public Boolean cerrarSesiondeUsuario() {
+			Iterator<Usuario> it = usuariosLogueados.iterator();
 
-	// NUEVO ELIMINAR , YA QUE ESTE S�LO ELIMINA AL USUARIO LOGUEADO
-	
-	public Boolean Eliminar (Integer Id,String password) {
-		if (usuarios.contains(usuarioLogueado)) {
-			if(usuarioLogueado.getPassword()==password && usuarioLogueado.getId()==Id) {
-			usuarios.remove(usuarioLogueado);
-			return true;
+			while (it.hasNext()) {
+
+				Usuario usuarioAuxiliar = it.next();
+				if (usuarioAuxiliar.equals(usuarioLogueado)) {
+					usuariosLogueados.remove(usuarioLogueado);
+				}return true;
 			}
-		}return false;
-	}
-	
+			return false;
+		}
 
-	
+		// BUSCAR USUARIOS ONLINE
+		public Boolean buscarUsuarioLogueado(Integer Id) throws SistemaExceptionNoEncuentraUsuario {
+			for (Usuario usuarioLogueado : usuariosLogueados) {
+				if (usuarioLogueado.getId().equals(Id))
+					return true;
+			}
+			throw new SistemaExceptionNoEncuentraUsuario();
+		}
+
+		// BUSCAR USUARIOS 
+		public Boolean buscarUsuarioNoLogueado(Integer Id) throws SistemaExceptionNoEncuentraUsuario {
+			for (Usuario usuarioIngresado1 : usuarios) {
+				if (usuarioIngresado1.getId().equals(Id)) {
+					return true;
+				}
+				throw new SistemaExceptionNoEncuentraUsuario();
+			}
+			throw new SistemaExceptionNoEncuentraUsuario();
+		}
+
+		// ELIMINAR USUARIO
+
+		public Boolean EliminarUsuario(Integer Id, String password) throws SistemaExceptionNoEliminaUsuario {
+			for (Usuario usuarioLogueado : usuariosLogueados) {
+				if (usuarioLogueado.getPassword() == password && usuarioLogueado.getId() == Id) {
+					Iterator<Usuario> it = usuarios.iterator();
+
+					while (it.hasNext()) {
+
+						Usuario usuario = it.next();
+						if (usuario.getId().equals(Id) && usuario.getPassword().equals(password)) {
+							usuarios.remove(usuario);
+							return true;
+						}
+					}
+				}
+				throw new SistemaExceptionNoEliminaUsuario();
+			}
+			throw new SistemaExceptionNoEliminaUsuario();
+		}
+
+		// NUEVO ELIMINAR , YA QUE ESTE SÓLO ELIMINA AL USUARIO LOGUEADO
+
+		public Boolean Eliminar(Integer Id, String password) throws SistemaExceptionNoEliminaUsuario {
+			if (usuarios.contains(usuarioLogueado)) {
+				if (usuarioLogueado.getPassword() == password && usuarioLogueado.getId() == Id) {
+					usuarios.remove(usuarioLogueado);
+					return true;
+				}
+			}
+			throw new SistemaExceptionNoEliminaUsuario();
+		}
 }
