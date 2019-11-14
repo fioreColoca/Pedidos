b060df733acfd946a6baba648d8fe10d748550db
fioreColoca <fiorecoloca99@gmail.com>
2019-11-14 19:25:51 -0300

    Update TestCliente.java

--- Restaurante/Test/Test/TestCliente.java
+++ Restaurante/Test/Test/TestCliente.java
@@ -313,4 +313,27 @@ public class TestCliente {
 		System.out.println("----------");
 	}
 	
+	@Test
+	public void verEstadoPedido() throws ExceptionProductoInexistente, SistemaExceptionNoCreaUsuario, SistemaExceptionNoEncuentraUsuario, AdministradorExceptionAgregarProducto {
+		Cliente c1 = new Cliente(123, "lucia", "martinez", "luciamartinez@hotmail.com", "123luciamartinez");
+		Sistema sistema = new Sistema();
+		sistema.crearNuevoUsuario(c1);
+		sistema.loguearUsuario("luciamartinez@hotmail.com", "123luciamartinez");
+		Administrador a1 = new Administrador(456, "Marcelo", "Gomez", "MarceloGomez123@gmail.com", "123123");
+		Producto p1 = new Producto(Categoria.PIZZAS, "Muzzarella", 500.0, 8);
+		Producto p2 = new Producto(Categoria.EMPANADAS, "pollo", 50.0, 4);
+		Producto p3 = new Producto(Categoria.VINOS, "Tinto", 500.0, 2);
+		Producto p4 = new Producto(Categoria.BEBIDAS, "Agua", 60.0, 6);
+		Restaurante r1 = new Restaurante("Cafeteria", 123456);
+		a1.agregarProductos(p1, r1);
+		a1.agregarProductos(p2, r1);
+		a1.agregarProductos(p3, r1);
+		a1.agregarProductos(p4, r1);
+		Pedido pedido = new Pedido (4,r1);
+	    c1.pedirProducto(4,pedido,r1);
+	    c1.pedirProducto(6,pedido,r1);
+	    c1.pedirProducto(4,pedido,r1);
+
+	    System.out.println("Estado del pedio " + c1.verEstado(pedido));
+	}
 }
