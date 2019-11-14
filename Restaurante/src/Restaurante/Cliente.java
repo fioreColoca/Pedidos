35ad9c67a0c0fec0e5cb335a6b1ef759a2738194
seredonato <serefeli@hotmail.com>
2019-11-14 17:31:48 -0300

    Update Cliente.java

--- Restaurante/src/Restaurante/Cliente.java
+++ Restaurante/src/Restaurante/Cliente.java
@@ -1,9 +1,23 @@
 package Restaurante;
 
+import java.util.LinkedList;
+
 public class Cliente extends Usuario {
 
 	public Cliente(Integer Id, String nombre, String apellido, String email, String password) {
 		super(Id, nombre, apellido, email, password);
+	}
+	
+	LinkedList<Producto> HitorialDePedidos = new LinkedList<Producto>();
+	LinkedList<Producto> PedidoActual = new LinkedList<Producto>();
+	
+	public Boolean agregarComida(Producto p, Restaurante r) {
+		if(!r.productos.contains(p)) {
+			this.PedidoActual.add(p);
+			return true;
+		}
+		
+		return false;
 	}
 
 
