8e86e224f3d83db0fc3cc104dae5f5c343fe0674
fioreColoca <fiorecoloca99@gmail.com>
2019-11-14 17:10:00 -0300

    Update Administrador.java

--- Restaurante/src/Restaurante2/Administrador.java
+++ Restaurante/src/Restaurante2/Administrador.java
@@ -5,74 +5,82 @@ import java.util.LinkedList;
 
 public class Administrador extends Usuario {
 
-	public Administrador(Integer Id, String nombre, String apellido, String email, String password) {
-		super(Id, nombre, apellido, email, password);
-	}
-	// Lo puse aca asi son metodos q hace solo el administrador//
 
-	public Boolean agregarProductos(Producto P, Restaurante nombreRestaurante) {
-		if (!nombreRestaurante.productos.contains(P)) {
-			nombreRestaurante.productos.add(P);
-			return true;
+		public Administrador(Integer Id, String nombre, String apellido, String email, String password) {
+			super(Id, nombre, apellido, email, password);
 		}
-		return false;
 
-	}
-	// Otra manera de hacerlo//
-	/*
-	 * public void agregarProductosSet(Producto P, Restaurante nombreRestaurante) {
-	 * nombreRestaurante.productos.add(P); }
-	 */
-
-	// Eliminar//
-
-	public Boolean eliminarProducto(Integer IdProducto, Restaurante R) {
-		Iterator<Producto> it = R.productos.iterator();
-
-		while (it.hasNext()) {
-			Producto producto = it.next();
-			if (producto.getIdProducto().equals(IdProducto)) {
-				R.productos.remove(producto);
+		// AGREGAR PRODUCTO
+		public Boolean agregarProductos(Producto P, Restaurante nombreRestaurante) throws AdministradorExceptionAgregarProducto {
+			if (!nombreRestaurante.productos.contains(P)) {
+				nombreRestaurante.productos.add(P);
 				return true;
 			}
+			throw new AdministradorExceptionAgregarProducto();
 		}
-		return false;
-	}
 
-	public Boolean buscarProducto(Integer id, Restaurante R) {
-		for (Producto p : R.productos) {
-			if (p.getIdProducto().equals(id))
-				return true;
-		}
-		return false;
-	}
+		// ELIMINAR PRODUCTO
 
-	public Boolean modificarPrecio(Double nuevoPrecio, Integer id, Restaurante R) {
-		for (Producto p : R.productos) {
-			if (p.getIdProducto().equals(id))
-				if (nuevoPrecio < 3000 && nuevoPrecio > 0) {
-					p.setPrecio(nuevoPrecio);
+		public Boolean eliminarProducto(Integer IdProducto, Restaurante R) throws AdministradorExceptionEliminarProducto {
+			Iterator<Producto> it = R.productos.iterator();
+
+			while (it.hasNext()) {
+				Producto producto = it.next();
+				if (producto.getIdProducto().equals(IdProducto)) {
+					R.productos.remove(producto);
 					return true;
 				}
-			return false;
-		}
-		return false;
-	}
-
-	public void finalizarPedido(Pedido p) {
-		p.setEstado(EstadoPedido.FINALIZADO);
-	}
-
-	public void pedidoEnProceso(Pedido p) {
-		p.setEstado(EstadoPedido.PROCESANDO);
-	}// DUDANDO COMO METER ESTO EN EL MAIN
-
-	public void mostrarListadoPedidosPendientes(Restaurante r1) {
-		for (Producto P : r1.productos) {
-			if (P == null) {
 			}
-			System.out.println(P.hashCode() + "-------" + P.toString());
-
+			throw new AdministradorExceptionEliminarProducto();
 		}
+		
+		// BUSCAR PRODUCTO
+		
+		public Boolean buscarProducto(Integer id, Restaurante R) throws AdministradorExceptionProductoNoEncontrado {
+			for (Producto p : R.productos) {
+				if (p.getIdProducto().equals(id))
+					return true;
+			}
+			throw new AdministradorExceptionProductoNoEncontrado();
+		}
+
+		// MODIFICAR PRECIO DEL PRODUCTO
+		
+		public Boolean modificarPrecio(Double nuevoPrecio, Integer id, Restaurante R) throws AdministradorExceptionNoModificaPrecio, AdministradorExceptionProductoNoEncontrado {
+			for (Producto p : R.productos) {
+				if (p.getIdProducto().equals(id)) {
+					if (nuevoPrecio < 3000 && nuevoPrecio > 0) {
+						p.setPrecio(nuevoPrecio);
+						return true;
+					}
+				throw new AdministradorExceptionNoModificaPrecio();
+			}
+			throw new AdministradorExceptionProductoNoEncontrado();
+		}
+			throw new AdministradorExceptionProductoNoEncontrado();
 	}
+		
+		// LISTADO DE PEDIDOS PENDIENTES
+		
+		public void mostrarListadoPedidosPendientes(Restaurante r1) {
+			for (Producto P : r1.productos) {
+				if (P == null) {
+				}
+				System.out.println(P.hashCode() + "-------" + P.toString());
+
+			}
+		}
+
+		// ESTADO DE PEDIDO FINALIZADO
+		
+		public void finalizarPedido(Pedido p) {
+			p.setEstado(EstadoPedido.FINALIZADO);
+		}
+		
+		//ESTADO DE PEDIDO EN PROCESO
+		
+		public void pedidoEnProceso(Pedido p) {
+			p.setEstado(EstadoPedido.PROCESANDO);
+		}// DUDANDO COMO METER ESTO EN EL MAIN
+
 }
