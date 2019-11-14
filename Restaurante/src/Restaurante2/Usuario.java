5856e3e77de37612cb7c0dbd137876ba385c89b0
fioreColoca <fiorecoloca99@gmail.com>
2019-11-14 15:54:27 -0300

    Update Usuario.java

--- Restaurante/src/Restaurante2/Usuario.java
+++ Restaurante/src/Restaurante2/Usuario.java
@@ -58,7 +58,7 @@ public abstract class Usuario {
 		this.password = password;
 	}
 
-	public Boolean validarPassword(String password) {
+	public Boolean validarPassword(String password) throws UsuarioExceptionPasswordIncorrecta {
 
 		if (password.length() >= 8) {
 			for (int i = 0; i < password.length(); i++) {
@@ -67,7 +67,7 @@ public abstract class Usuario {
 				}
 			}
 		}
-		return false;
+		throw new UsuarioExceptionPasswordIncorrecta();
 	}
 	
 	
