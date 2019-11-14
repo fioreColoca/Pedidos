d8b51b0c05aee9a0fbba77cfbf28bdd947ac5adf
fioreColoca <fiorecoloca99@gmail.com>
2019-11-14 11:11:06 -0300

    Create MAIN.java

---
+++ Restaurante/src/Restaurante2/MAIN.java
@@ -0,0 +1,16 @@
+package Restaurante2;
+
+import java.util.Scanner;
+
+public class MAIN {
+
+	public static void main(String[] args) {
+		/*String selector;
+		Scanner teclado0 = new Scanner (System.in);
+		do {
+			System.out.println ("Bienvenido , para comenzar ingrese si es un cliente seleccione la letra ->A<- o un administrador ->B<-");            
+			
+		}while ();*/
+	}
+
+}
