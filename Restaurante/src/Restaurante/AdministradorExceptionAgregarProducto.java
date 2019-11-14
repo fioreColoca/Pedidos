46ee731c6f42c6ea9a10812ccecfce52ffc7afa5
seredonato <serefeli@hotmail.com>
2019-11-14 15:18:47 -0300

    Create AdministradorExceptionAgregarProducto.java

---
+++ Restaurante/src/Restaurante/AdministradorExceptionAgregarProducto.java
@@ -0,0 +1,8 @@
+package Restaurante;
+
+public class AdministradorExceptionAgregarProducto extends Exception {
+	public AdministradorExceptionAgregarProducto() {
+		super("No se pudo agregar el Producto");
+	}
+
+}
