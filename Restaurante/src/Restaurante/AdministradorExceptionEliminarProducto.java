3b5d75d377d87d052508b9e4e63c1fbc9a28ee72
seredonato <serefeli@hotmail.com>
2019-11-14 15:18:50 -0300

    Create AdministradorExceptionEliminarProducto.java

---
+++ Restaurante/src/Restaurante/AdministradorExceptionEliminarProducto.java
@@ -0,0 +1,7 @@
+package Restaurante;
+
+public class AdministradorExceptionEliminarProducto extends Exception {
+	public AdministradorExceptionEliminarProducto() {
+		super("No se pudo Eliminar el Producto");
+	}
+}
