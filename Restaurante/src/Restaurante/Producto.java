21769ef86fc5b00acaa7ad39a19e5c03a1d2864e
seredonato <serefeli@hotmail.com>
2019-11-14 17:17:46 -0300

    Update Producto.java

--- Restaurante/src/Restaurante/Producto.java
+++ Restaurante/src/Restaurante/Producto.java
@@ -1,16 +1,16 @@
 package Restaurante;
 
-public class Producto {
+public class Producto implements Comparable<Producto> {
 	private Categoria categoria;
 	private String descripcion;
 	private Double precio;
 	private Integer idProducto;
-	
-	public Producto (Categoria categoria,String descripcion,Double precio,Integer idProducto) {
-		this.categoria=categoria;
-		this.descripcion=descripcion;
-		this.precio=precio;
-		this.idProducto=idProducto;
+
+	public Producto(Categoria categoria, String descripcion, Double precio, Integer idProducto) {
+		this.categoria = categoria;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.idProducto = idProducto;
 	}
 
 	public Categoria getCategoria() {
@@ -35,8 +35,9 @@ public class Producto {
 
 	public void setPrecio(Double precio) {
 		this.precio = precio;
-		
+
 	}
+
 	public Integer getIdProducto() {
 		return idProducto;
 	}
@@ -44,5 +45,58 @@ public class Producto {
 	public void setPrecio(Integer id) {
 		this.idProducto = id;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Producto [categoria=" + categoria + ", descripcion=" + descripcion + ", precio=" + precio
+				+ ", idProducto=" + idProducto + "]";
+	}
+
+	// identificacion unica
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((categoria == null) ? 0 : categoria.hashCode());
+		result = prime * result + ((descripcion == null) ? 0 : descripcion.hashCode());
+		result = prime * result + ((idProducto == null) ? 0 : idProducto.hashCode());
+		result = prime * result + ((precio == null) ? 0 : precio.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Producto other = (Producto) obj;
+		if (categoria != other.categoria)
+			return false;
+		if (descripcion == null) {
+			if (other.descripcion != null)
+				return false;
+		} else if (!descripcion.equals(other.descripcion))
+			return false;
+		if (idProducto == null) {
+			if (other.idProducto != null)
+				return false;
+		} else if (!idProducto.equals(other.idProducto))
+			return false;
+		if (precio == null) {
+			if (other.precio != null)
+				return false;
+		} else if (!precio.equals(other.precio))
+			return false;
+		return true;
+	}
+
+	// ORDENA EL LISTADO SEGUN EL ID,NO PUEDE HABER ID REPETIDOS
+	@Override
+	public int compareTo(Producto idPedido) {
+		return this.idProducto.compareTo(idPedido.getIdProducto());
+	}
+
 }
