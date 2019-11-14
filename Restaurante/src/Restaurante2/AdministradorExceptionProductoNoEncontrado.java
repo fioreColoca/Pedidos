96656ff869dd385a5ba41eb8eb832ba5462728c0
fioreColoca <fiorecoloca99@gmail.com>
2019-11-14 17:10:11 -0300

    Create AdministradorExceptionProductoNoEncontrado.java

---
+++ Restaurante/src/Restaurante2/AdministradorExceptionProductoNoEncontrado.java
@@ -0,0 +1,7 @@
+package Restaurante2;
+
+public class AdministradorExceptionProductoNoEncontrado extends Exception {
+	public AdministradorExceptionProductoNoEncontrado() {
+		super("No se encontró el Producto");
+	}
+}
