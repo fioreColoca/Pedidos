9f0e2855882d24bb163927b220fd8aa370c1734f
seredonato <serefeli@hotmail.com>
2019-11-17 19:02:47 -0300

    Update MAIN.java

--- Restaurante/src/Restaurante/MAIN.java
+++ Restaurante/src/Restaurante/MAIN.java
@@ -4,20 +4,21 @@ import java.util.Scanner;
 
 public class MAIN {
 
-	public static void main(String[] args) throws UsuarioExceptionPasswordIncorrecta {
-		Integer optcion = null;
+	public static void main(String[] args) throws UsuarioExceptionPasswordIncorrecta, MainExceptionSoloCaracteres {
+		Integer option = null;
 		Scanner teclado = new Scanner(System.in);
 		Sistema sistema = new Sistema();
 		Boolean salir = false;
+		
 // <----------MENU ---------- >
 		do {
 			System.out.println("Ingrese '1' si es administrador o '2' si es cliente");
-			optcion = teclado.nextInt();
-			if (optcion != 1 || optcion != 2) {
+			option = teclado.nextInt();
+			if (option != 1 || option != 2) {
 				salir = true;
 			}
 
-			switch (optcion) {
+			switch (option) {
 			case 1:
 
 				Boolean salirAdministrador = false;
@@ -33,6 +34,7 @@ public class MAIN {
 					switch (opcionAdministrador) {
 
 					case 1:
+						Boolean error = false;
 						String nombre;
 						String apellido;
 						String email;
@@ -41,8 +43,24 @@ public class MAIN {
 						// *HACER CLASE PARA QUE VERIFIQUE SI ESTAN BIEN O NO LO INGRESADO //
 
 						System.out.println("Selecciono para registrarse");
-						System.out.println("Ingrese su nombre");
-						nombre = teclado.next();
+						do {
+							System.out.println("Ingrese su nombre");
+							nombre = teclado.next();
+							if (sistema.soloLetras(nombre) == true) {
+								String nombreConfirmado = nombre;
+								System.out.println("Ingrese su apellido");
+								apellido = teclado.next();
+								if (sistema.soloLetras(apellido) == true) {
+									String apellidoConfrimado = apellido;
+								} else {
+									error = true;
+								}
+							} else {
+								error = true;
+							}
+
+						} while (error = false);
+
 						System.out.println("Ingrese su apellido");
 						apellido = teclado.next();
 						System.out.println("Ingrese su email");
@@ -53,7 +71,6 @@ public class MAIN {
 						id = teclado.nextInt();
 
 						Administrador a1 = new Administrador(id, nombre, apellido, email, password);
-						a1.setPassword(password);
 
 						// VERIFICAR PASSWORD ESTA MAL DENTRODE USUARIO
 
@@ -66,7 +83,7 @@ public class MAIN {
 			case 2:
 				Integer opcionCliente = null;
 				System.out.println("Seleccionaste cliente, Ingrese '1'  registrarse,'2' abrir sesión , '3' salir");
-				optcion = teclado.nextInt();
+				option = teclado.nextInt();
 
 				break;
 			}
