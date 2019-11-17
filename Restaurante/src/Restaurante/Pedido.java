9a3f8b6cc33be576131a2d5001ee833b74789cba
seredonato <serefeli@hotmail.com>
2019-11-17 17:58:58 -0300

    Update Pedido.java

--- Restaurante/src/Restaurante/Pedido.java
+++ Restaurante/src/Restaurante/Pedido.java
@@ -1,36 +1,28 @@
 package Restaurante;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Pedido {
-	
-	private LinkedList<Producto> pedidos = new LinkedList<>();
 
-	private Cliente cliente;
+	ArrayList<Producto> pedidos = new ArrayList<Producto>();
 	private Restaurante restaurante;
+	private Integer mesa;
 	private Integer numeroDePedido;
 	private EstadoPedido estado;
 
-	public Pedido(Cliente cliente, Restaurante restaurante) {
-		this.cliente = cliente;
+	public Pedido(Integer mesa, Restaurante restaurante) {
+		this.mesa = mesa;
 		this.restaurante = restaurante;
-		this.estado = EstadoPedido.FINALIZADO;
+		this.estado = EstadoPedido.INICIALIZANDO;
 	}
 
-	public Cliente getCliente() {
-		return cliente;
-	}
-
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
+	public Integer getMesa() {
+		return mesa;
 	}
 
 	public Restaurante getRestaurante() {
 		return restaurante;
-	}
-
-	public void setRestaurante(Restaurante restaurante) {
-		this.restaurante = restaurante;
 	}
 
 	public Integer getNumeroDePedido() {
@@ -49,15 +41,122 @@ public class Pedido {
 		this.estado = estado;
 	}
 
-	public LinkedList<Producto> getPedidos() {
+	public ArrayList<Producto> getPedidos() {
 		return pedidos;
 	}
 
-	public void setPedidos(LinkedList<Producto> pedidos) {
+	public void setPedidos(ArrayList<Producto> pedidos) {
 		this.pedidos = pedidos;
 	}
 
-	public void mostrarPedidos() { //NOSE COMO SE HACE 
+	public void mostrarPedidos() { 
+		this.pedidos.toString();
+	}
+
+	public void agregarProductos() {
+
+	}
+
+	@Override
+	public String toString() {
+		return "Pedido [pedidos=" + pedidos + ", restaurante=" + restaurante + ", mesa=" + mesa + ", numeroDePedido="
+				+ numeroDePedido + ", estado=" + estado + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((estado == null) ? 0 : estado.hashCode());
+		result = prime * result + ((mesa == null) ? 0 : mesa.hashCode());
+		result = prime * result + ((numeroDePedido == null) ? 0 : numeroDePedido.hashCode());
+		result = prime * result + ((pedidos == null) ? 0 : pedidos.hashCode());
+		result = prime * result + ((restaurante == null) ? 0 : restaurante.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		if (estado != other.estado)
+			return false;
+		if (mesa == null) {
+			if (other.mesa != null)
+				return false;
+		} else if (!mesa.equals(other.mesa))
+			return false;
+		if (numeroDePedido == null) {
+			if (other.numeroDePedido != null)
+				return false;
+		} else if (!numeroDePedido.equals(other.numeroDePedido))
+			return false;
+		if (pedidos == null) {
+			if (other.pedidos != null)
+				return false;
+		} else if (!pedidos.equals(other.pedidos))
+			return false;
+		if (restaurante == null) {
+			if (other.restaurante != null)
+				return false;
+		} else if (!restaurante.equals(other.restaurante))
+			return false;
+		return true;
+	}
+
+	public Boolean pedirProducto(Integer id, Restaurante R) {
+		for (Producto p : R.productos) {
+			if (p.getIdProducto().equals(id)) {
+				this.pedidos.add(p);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public Double pedirCuenta(Restaurante R) {
+		Double acumuladorPrecio = 0.0;
+		for (Producto p : pedidos) {
+			acumuladorPrecio += p.getPrecio();
+
+		}
+		return acumuladorPrecio;
+
+	}
+
+	public void eliminarPedido() {
+		pedidos.clear();
+	}
+
+	public void pagar() { // NO FUNCIONA
+		pedidos.clear();
+	}
+
+	public boolean eliminarProducto(Integer idProducto) {
+		Iterator<Producto> it = pedidos.iterator();
+
+		while (it.hasNext()) {
+
+			Producto pedido = it.next();
+			if (pedido.getIdProducto().equals(idProducto)) {
+				pedidos.remove(pedido);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void mostrarPedido() {
+		for (Producto P : pedidos) {
+			if (P == null) {
+			}
+			System.out.println(P.hashCode() + "-------" + P.toString());
+		}
 	}
 
 }
