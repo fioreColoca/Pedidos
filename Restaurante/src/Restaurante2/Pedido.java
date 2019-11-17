4675e8cf710958dbacefcf6343ce637f53b2fec5
fioreColoca <fiorecoloca99@gmail.com>
2019-11-17 19:44:09 -0300

    Update Pedido.java

--- Restaurante/src/Restaurante2/Pedido.java
+++ Restaurante/src/Restaurante2/Pedido.java
@@ -7,30 +7,30 @@ import java.util.Iterator;
 public class Pedido {
 
 	ArrayList<Producto> pedidos = new ArrayList<Producto>();
-	
+
 	private Restaurante restaurante;
 	private Integer mesa;
 	private Integer numeroDePedido;
 	private static int ultimo = 0;
-    public final int id;
-	
+	public final int id;
+
 	private EstadoPedido estado;
 
-	public Pedido(Integer mesa,Restaurante restaurante) {
-		this.id= ultimo++;
+	public Pedido(Integer mesa, Restaurante restaurante) {
+		this.id = ultimo++;
 		this.mesa = mesa;
 		this.restaurante = restaurante;
 		this.estado = EstadoPedido.INICIALIZANDO;
-	}	
+	}
 
 	public Integer getMesa() {
 		return mesa;
 	}
-	
+
 	public Restaurante getRestaurante() {
 		return restaurante;
 	}
-	
+
 	public Integer getNumeroDePedido() {
 		return numeroDePedido;
 	}
@@ -47,7 +47,7 @@ public class Pedido {
 		this.estado = estado;
 	}
 
-	public ArrayList <Producto> getPedidos() {
+	public ArrayList<Producto> getPedidos() {
 		return pedidos;
 	}
 
@@ -55,21 +55,74 @@ public class Pedido {
 		this.pedidos = pedidos;
 	}
 
-	public void mostrarPedidos() { //NOSE COMO SE HACE 
+	public void mostrarPedidos() {
+		this.pedidos.toString();
 	}
-	
-	
+
 	public void agregarProductos() {
-		
+
 	}
-	
+
+	public Boolean pedirProducto(Integer id, Restaurante R) {
+		for (Producto p : R.productos) {
+			if (p.getIdProducto().equals(id)) {
+				this.pedidos.add(p);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public Double pedirCuenta(Restaurante R) {
+		Double acumuladorPrecio = 0.0;
+		for (Producto p : pedidos) {
+			acumuladorPrecio += p.getPrecio();
+
+		}
+		return acumuladorPrecio;
+
+	}
+
+	public void eliminarPedido() {
+		pedidos.clear();
+	}
+
+	public void pagar() {
+		pedidos.clear();
+	}
+
+	public Boolean eliminarProducto(Integer idProducto) throws ExceptionProductoInexistente{
+		Iterator<Producto> it = pedidos.iterator();
+
+		while (it.hasNext()) {
+
+			Producto pedido = it.next();
+			if (pedido.getIdProducto().equals(idProducto)) {
+				pedidos.remove(pedido);
+				return true;
+			}
+		}
+		 throw new ExceptionProductoInexistente();
+	}
+
+	public void mostrarPedido() {
+		for (Producto P : pedidos) {
+			if (P == null) {
+			}
+			System.out.println(P.hashCode() + "-------" + P.toString());
+		}
+	}
+
+	public void enviarPedido() {
+		this.estado = EstadoPedido.ENVIADO;
+
+	}
+
 	@Override
 	public String toString() {
 		return "Pedido [pedidos=" + pedidos + ", restaurante=" + restaurante + ", mesa=" + mesa + ", numeroDePedido="
 				+ numeroDePedido + ", estado=" + estado + "]";
 	}
-
-
 
 	@Override
 	public int hashCode() {
@@ -82,8 +135,6 @@ public class Pedido {
 		result = prime * result + ((restaurante == null) ? 0 : restaurante.hashCode());
 		return result;
 	}
-
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -117,64 +168,6 @@ public class Pedido {
 		} else if (!restaurante.equals(other.restaurante))
 			return false;
 		return true;
-	}
-	public Boolean pedirProducto (Integer id, Restaurante R) {
-		for (Producto p : R.productos) {
-			if (p.getIdProducto().equals(id)) {
-				this.pedidos.add(p);
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public Double pedirCuenta(Restaurante R)  {
-		Double acumuladorPrecio = 0.0;
-		for (Producto p : pedidos) {
-				acumuladorPrecio += p.getPrecio();
-
-			}return acumuladorPrecio;
-
-	}
-	
-	
-	public void eliminarPedido()   {
-		pedidos.clear();
-	}
-	
-	public void pagar() { //NO FUNCIONA
-		pedidos.clear();
-		}
-		
-	
-
-	public boolean eliminarProducto (Integer idProducto) {
-		Iterator<Producto> it = pedidos.iterator();
-
-		while (it.hasNext()) {
-
-			Producto pedido = it.next();
-			if (pedido.getIdProducto().equals(idProducto)) {
-				pedidos.remove(pedido);
-				return true;
-			}
-		}return false;
-	}
-	
-	
-	public void mostrarPedido() {
-		for (Producto P : pedidos) {
-			if (P == null) {
-			}
-			System.out.println(P.hashCode() + "-------" + P.toString());
-		}
-	}
-		
-	
-	
-	public void enviarPedido() {
-		this.estado = EstadoPedido.ENVIADO;
-		
 	}
 	
 
