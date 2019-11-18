a4a49b7d955ddf58dbd5dd41d584c3591f244cbb
seredonato <serefeli@hotmail.com>
2019-11-17 21:35:09 -0300

    Update MAIN.java

--- Restaurante/src/Restaurante/MAIN.java
+++ Restaurante/src/Restaurante/MAIN.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class MAIN {
 
-	public static void main(String[] args) throws UsuarioExceptionPasswordIncorrecta, MainExceptionSoloCaracteres {
-		Integer option = null;
+	public static void main(String[] args)
+			throws UsuarioExceptionPasswordIncorrecta, MainExceptionSoloCaracteres, SistemaExceptionNoEncuentraUsuario {
+		Integer optcion = null;
 		Scanner teclado = new Scanner(System.in);
 		Sistema sistema = new Sistema();
 		Boolean salir = false;
-		
 // <----------MENU ---------- >
 		do {
 			System.out.println("Ingrese '1' si es administrador o '2' si es cliente");
-			option = teclado.nextInt();
-			if (option != 1 || option != 2) {
+			optcion = teclado.nextInt();
+			if (optcion != 1 || optcion != 2) {
 				salir = true;
 			}
 
-			switch (option) {
+			switch (optcion) {
 			case 1:
 
 				Boolean salirAdministrador = false;
@@ -33,57 +33,130 @@ public class MAIN {
 					}
 					switch (opcionAdministrador) {
 
-					case 1:
+					case 1: {
 						Boolean error = false;
 						String nombre;
 						String apellido;
-						String email;
 						String password;
-						Integer id;
+						String id;
+						String email;
+						String nombreConfirmado = null;
+						String apellidoConfirmado = null;
+						String emailConfirmado = null;
+						String passwordConfirmado = null;
+						String idConfirmado = null;
 						// *HACER CLASE PARA QUE VERIFIQUE SI ESTAN BIEN O NO LO INGRESADO //
 
 						System.out.println("Selecciono para registrarse");
+
+						boolean ingresoTerminado;
 						do {
-							System.out.println("Ingrese su nombre");
-							nombre = teclado.next();
-							if (sistema.soloLetras(nombre) == true) {
-								String nombreConfirmado = nombre;
-								System.out.println("Ingrese su apellido");
-								apellido = teclado.next();
-								if (sistema.soloLetras(apellido) == true) {
-									String apellidoConfrimado = apellido;
-								} else {
-									error = true;
+							ingresoTerminado = false;
+							do {
+								System.out.println("Ingrese su nombre");
+								nombre = teclado.next();
+								try {
+									if (sistema.soloLetras(nombre) == true) {
+										nombreConfirmado = nombre;
+										do {
+											System.out.println("Ingrese su apellido");
+											apellido = teclado.next();
+
+											try {
+												if (sistema.soloLetras(apellido) == true) {
+													apellidoConfirmado = apellido;
+													do {
+														System.out.println("Ingrese su email");
+														email = teclado.next();
+														try {
+															if (sistema.verificarEmail(email) == true) {
+																emailConfirmado = email;
+																do {
+																	System.out.println("Ingrese su contraseña");
+																	password = teclado.next();
+																	try {
+																		if (sistema.validarPassword(password) == true) {
+																			passwordConfirmado = password;
+																			do {
+																				System.out.println("Ingrese su dni");
+																				id = teclado.next();
+																				try {
+																					if (sistema
+																							.soloNumeros(id) == true) {
+																						idConfirmado = id;
+																						ingresoTerminado = true;
+																						System.out.println(
+																								"Te registraste correctamente");
+
+																						// FALTAALGO QUE LO CORTE
+																					}
+																				} catch (ExceptionNumero e) {
+																					System.out.println(e.getMessage());
+																				}
+
+																			} while (error != true); // FALTA ALGO PARA
+																										// QUE LO CORTE
+																		}
+																	} catch (UsuarioExceptionPasswordIncorrecta e) {
+																		System.out.println(e.getMessage());
+																	}
+
+																} while (error != true);
+															}
+
+														} catch (ExceptionEmail e) {
+															System.out.println(e.getMessage());
+														}
+
+													} while (error != true);
+
+												}
+
+											} catch (MainExceptionSoloCaracteres e) {
+												System.out.println(e.getMessage());
+											}
+										} while (error != true);
+									}
+
+								} catch (MainExceptionSoloCaracteres e) {
+									System.out.println(e.getMessage());
 								}
-							} else {
-								error = true;
-							}
 
-						} while (error = false);
+							} while (error != true);
 
-						System.out.println("Ingrese su apellido");
-						apellido = teclado.next();
-						System.out.println("Ingrese su email");
-						email = teclado.next();
-						System.out.println("Ingrese su password");
-						password = teclado.next();
-						System.out.println("Ingrese su DNI, sólo números");
-						id = teclado.nextInt();
+						} while (ingresoTerminado == true);
 
-						Administrador a1 = new Administrador(id, nombre, apellido, email, password);
+						Administrador a1 = new Administrador(idConfirmado, nombreConfirmado, apellidoConfirmado,
+								emailConfirmado, passwordConfirmado);
 
-						// VERIFICAR PASSWORD ESTA MAL DENTRODE USUARIO
+						break;
 
 					}
 
-					break;
+					// VERIFICAR PASSWORD ESTA MAL DENTRODE USUARIO
+					case 2: {
+						String email2;
+						String password2;
+						Integer opcionCliente = null;
+						System.out.println("Ingrese email");
+						email2 = teclado.next();
+						System.out.println("Ingrese email");
+						password2 = teclado.next();
+
+						sistema.loguearUsuario("email2", "password2");
+
+						break;
+					}
+
+					}
+
 				} while (salirAdministrador = true);
 				break;
 
 			case 2:
 				Integer opcionCliente = null;
 				System.out.println("Seleccionaste cliente, Ingrese '1'  registrarse,'2' abrir sesión , '3' salir");
-				option = teclado.nextInt();
+				optcion = teclado.nextInt();
 
 				break;
 			}
