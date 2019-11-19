95094a66b3d8ab6b33f321bbc8e04a44943aec02
fioreColoca <fiorecoloca99@gmail.com>
2019-11-19 16:08:47 -0300

    Update TestRestaurante.java

--- Restaurante/Test/Test/TestRestaurante.java
+++ Restaurante/Test/Test/TestRestaurante.java
@@ -21,10 +21,10 @@ public class TestRestaurante {
 			sistema.crearNuevoUsuario(c1);
 			sistema.loguearUsuario("luciamartinez@hotmail.com", "123luciamartinez");
 			Administrador a1 = new Administrador("456", "Marcelo", "Gomez", "MarceloGomez123@gmail.com", "123123");
-			Producto p1 = new Producto(Categoria.PIZZAS, "Muzzarella", 500.0, 2);
-			Producto p2 = new Producto(Categoria.EMPANADAS, "pollo", 50.0, 4);
-			Producto p3 = new Producto(Categoria.VINOS, "Tinto", 500.0, 5);
-			Producto p4 = new Producto(Categoria.BEBIDAS, "Agua", 60.0, 6);
+			Producto p1 = new Producto(Categoria.PIZZAS, "Muzzarella", 500, 2);
+			Producto p2 = new Producto(Categoria.EMPANADAS, "pollo", 50, 4);
+			Producto p3 = new Producto(Categoria.VINOS, "Tinto", 500, 5);
+			Producto p4 = new Producto(Categoria.BEBIDAS, "Agua", 60, 6);
 			Restaurante r1 = new Restaurante("Cafeteria", "123456");
 			a1.agregarProductos(p1, r1);
 			a1.agregarProductos(p2, r1);
