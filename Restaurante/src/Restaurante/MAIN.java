83dd70ffe588d4d3407d677651fdad34e6b20bb4
seredonato <serefeli@hotmail.com>
2019-11-19 15:18:38 -0300

    Update MAIN.java

--- Restaurante/src/Restaurante/MAIN.java
+++ Restaurante/src/Restaurante/MAIN.java
@@ -11,9 +11,9 @@ public class MAIN {
 		String opcion = null;
 		Scanner teclado = new Scanner(System.in);
 		Sistema sistema = new Sistema();
-		// Restaurante r1 = new Restaurante(" ", " ");
-		// Cliente c1 = new Cliente(null, "", "", "", "");
-		// Administrador a1 = new Administrador(null, "", "", "", "");
+		Restaurante r1 = new Restaurante(" ", " ");
+		Cliente c1 = new Cliente(null, "", "", "", "");
+		Administrador a1 = new Administrador(null, "", "", "", "");
 		Boolean salir = false;
 
 		// <----------MENU ---------- >
@@ -47,6 +47,155 @@ public class MAIN {
 						sistema.loguearUsuario(email, password);
 						System.out.println("Usted ha iniciado sesion correctamente");
 						salir = true;
+
+						// ACA IRIA LA CONFIMACION DE QUE ES ADMINISTRADOR O CLIENTE
+						String opcionADM;
+						do {
+
+							// MENU DE ADMINISTRADOR
+							System.out.println("¿Que desea hacer?");
+
+							System.out.println("1 -> Agregar Productos");
+							System.out.println("2 -> Eliminar Producto");
+							System.out.println("3 -> Buscar Producto");
+							System.out.println("4 -> Modificar Precio del Producto");
+							System.out.println("5 -> Cerrar Sesion");
+							System.out.println("6 -> Eliminar Cuenta");
+							System.out.println("7 -> Menu Principal");
+							opcionADM = teclado.next();
+
+							if (opcionADM != "1" || opcionADM != "2" || opcionADM != "3" || opcionADM != "4"
+									|| opcionADM != "5" || opcionADM != "6") {
+								salir = true;
+							}
+
+							// DEL MENU DEL ADM
+							switch (opcionADM) {
+
+							case "1":
+
+								String opcionADM1 = null;
+								Categoria categoria = null;
+								String descripcion = null;
+								String descripcionConfirmado = null;
+								Integer precioConfirmado = null;
+								Integer idConfirmada = null;
+								Integer precio;
+								Integer id1 = null;
+								Boolean errorProd = false;
+
+								System.out.println("Seleccionaste 1- 'Agregar Productos'");
+
+								do {
+									System.out
+											.println("Por favor, ingrese la categoria a la que agregara el producto: ");
+
+									System.out.println("1 -> PIZZAS");
+									System.out.println("2 -> EMPANADAS");
+									System.out.println("3 -> PASTAS");
+									System.out.println("4 -> POSTRES");
+									System.out.println("5 -> BEBIDAS");
+									System.out.println("6 -> VINOS");
+									System.out.println("7 -> ENSALADAS");
+									System.out.println("8 -> ENTRADAS");
+									System.out.println("9 -> MILANESAS");
+									System.out.println("10 -> GUARNICIONES");
+									System.out.println("11 -> PARRILLA");
+
+									opcionADM1 = teclado.next();
+
+									if (opcionADM1 != "1" || opcionADM1 != "2" || opcionADM1 != "3" || opcionADM1 != "4"
+											|| opcionADM1 != "5" || opcionADM1 != "6") {
+										salir = true;
+									}
+
+									else {
+
+										switch (opcionADM1) {
+										case "1":
+											categoria = Categoria.PIZZAS;
+											break;
+										case "2":
+											categoria = Categoria.EMPANADAS;
+											break;
+
+										case "3":
+											categoria = Categoria.PASTAS;
+											break;
+
+										case "4":
+											categoria = Categoria.POSTRES;
+											break;
+
+										case "5":
+											categoria = Categoria.BEBIDAS;
+											break;
+										case "6":
+											categoria = Categoria.VINOS;
+											break;
+										case "7":
+											categoria = Categoria.ENSALADAS;
+											break;
+
+										case "8":
+											categoria = Categoria.ENTRADAS;
+											break;
+
+										case "9":
+											categoria = Categoria.MILANESAS;
+											break;
+
+										case "10":
+											categoria = Categoria.GUARNICIONES;
+											break;
+
+										case "11":
+											categoria = Categoria.PARRILLA;
+											break;
+
+										}
+									}
+
+									do {
+										System.out.println("Ingrese descripcion");
+										descripcion = teclado.next();
+										try {
+											if (sistema.soloLetras(descripcion) == true) {
+												descripcionConfirmado = descripcion;
+											}
+											do {
+												System.out.println("Ingrese precio '000.00' ");
+												precio = (int) teclado.nextDouble();
+												precioConfirmado = precio;
+
+												System.out.println("Ingrese id sólo números ");
+												id1 = teclado.nextInt();
+												idConfirmada = id1;
+
+												try {
+													Producto p1 = new Producto(categoria, descripcionConfirmado,
+															precioConfirmado, idConfirmada);
+													if (a1.agregarProductos(p1, r1) == true) {
+														System.out.println("Ha creado su producto Exitosamente");
+													}
+
+												} catch (AdministradorExceptionAgregarProducto e) {
+													System.out.println(e.getMessage());
+												}
+
+											} while (errorProd != true);
+
+										} catch (MainExceptionSoloCaracteres e) {
+											System.out.println(e.getMessage());
+										}
+
+									} while (errorProd != true);
+
+								} while (salir = true);
+
+							}// cierra switch adm acciones
+
+						} while (salir = true);
 
 					} catch (SistemaExceptionNoEncuentraUsuario e) { // NO ENCUENTRA USUARIO
 						System.out.println(e.getMessage());
@@ -124,24 +273,20 @@ public class MAIN {
 												idAdmConfirmado = idAdm;
 											}
 
-											try {
-												/*
-												 * a1.setNombre(nombreAdm); a1.setApellido(apellidoAdm);
-												 * a1.setEmail(emailAdm); a1.setPassword(passwordAdm); a1.setId(idAdm);
-												 */
+											a1.setNombre(nombreAdmConfirmado);
+											a1.setApellido(apellidoAdmConfirmado);
+											a1.setEmail(emailAdmConfirmado);
+											a1.setPassword(passwordAdmConfirmado);
+											a1.setId(idAdmConfirmado);
 
-												Administrador a1 = new Administrador(idAdmConfirmado,
-														nombreAdmConfirmado, apellidoAdmConfirmado, emailAdmConfirmado,
-														passwordAdmConfirmado);
+											if (a1.getNombre().equals(nombreAdmConfirmado)
+													&& a1.getApellido().equals(apellidoAdmConfirmado)
+													&& a1.getId().equals(idAdmConfirmado)
+													&& a1.getEmail().equals(emailAdmConfirmado)
+													&& a1.getPassword().equals(passwordAdmConfirmado)) {
 
-												if (sistema.crearNuevoUsuario(a1) == true) {
-													System.out.println("Te registraste correctamente");
+												System.out.println("Te registraste correctamente");
 
-												}
-
-											} catch (SistemaExceptionNoCreaUsuario e) {
-												System.out.println(e.getMessage());
-												salirAdm = false;
 											}
 
 										} catch (ExceptionNumero e) {
@@ -225,24 +370,20 @@ public class MAIN {
 												idClieConfirmado = idClie;
 											}
 
-											try {
-												/*
-												 * a1.setNombre(nombreAdm); a1.setApellido(apellidoAdm);
-												 * a1.setEmail(emailAdm); a1.setPassword(passwordAdm); a1.setId(idAdm);
-												 */
+											c1.setNombre(nombreClieConfirmado);
+											c1.setApellido(apellidoClieConfirmado);
+											c1.setEmail(emailClieConfirmado);
+											c1.setPassword(passwordClieConfirmado);
+											c1.setId(idClieConfirmado);
 
-												Cliente a1 = new Cliente(idClieConfirmado, nombreClieConfirmado,
-														apellidoClieConfirmado, emailClieConfirmado,
-														passwordClieConfirmado);
+											if (c1.getNombre().equals(nombreClieConfirmado)
+													&& c1.getApellido().equals(apellidoClieConfirmado)
+													&& c1.getEmail().equals(emailClieConfirmado)
+													&& c1.getId().equals(idClieConfirmado)
+													&& c1.getPassword().equals(passwordClieConfirmado)) {
 
-												if (sistema.crearNuevoUsuario(a1) == true) {
-													System.out.println("Te registraste correctamente");
+												System.out.println("Te registraste correctamente");
 
-												}
-
-											} catch (SistemaExceptionNoCreaUsuario e) {
-												System.out.println(e.getMessage());
-												salirClie = false;
 											}
 
 										} catch (ExceptionNumero e) {
