7d432a6813bb1c5baf0b79fece1e044035c02809
fioreColoca <fiorecoloca99@gmail.com>
2019-11-26 21:59:44 -0300

    Delete SistemaExceptionNoEliminaUsuario.java

--- Restaurante/src/Restaurante2/SistemaExceptionNoEliminaUsuario.java
+++
@@ -1,9 +0,0 @@
-package Restaurante2;
-
-public class SistemaExceptionNoEliminaUsuario extends Exception{
-	
-	public SistemaExceptionNoEliminaUsuario() {
-		super("No se pudo eliminar el usuario");
-	}
-
-}
