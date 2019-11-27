0a3cc895a90ae2a57f3a0a968f33ddc25b9bc7cf
fioreColoca <fiorecoloca99@gmail.com>
2019-11-26 21:59:01 -0300

    Delete AdministradorExceptionNoModificaPrecio.java

--- Restaurante/src/Restaurante2/AdministradorExceptionNoModificaPrecio.java
+++
@@ -1,8 +0,0 @@
-package Restaurante2;
-
-public class AdministradorExceptionNoModificaPrecio extends Exception {
-	public AdministradorExceptionNoModificaPrecio() {
-		super("No se pudo modificar el precio del Producto");
-	}
-
-}
