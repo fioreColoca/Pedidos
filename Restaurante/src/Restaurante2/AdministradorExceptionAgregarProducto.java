e06c951ca0b5261d93412f42b04e91d937c2837a
fioreColoca <fiorecoloca99@gmail.com>
2019-11-26 21:58:57 -0300

    Delete AdministradorExceptionAgregarProducto.java

--- Restaurante/src/Restaurante2/AdministradorExceptionAgregarProducto.java
+++
@@ -1,7 +0,0 @@
-package Restaurante2;
-
-public class AdministradorExceptionAgregarProducto extends Exception {
-	public AdministradorExceptionAgregarProducto() {
-		super("No se pudo agregar el Producto");
-	}
-}
