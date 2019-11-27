ac528529ca7a750fe98941bee7e2d594fe49585f
fioreColoca <fiorecoloca99@gmail.com>
2019-11-26 21:59:37 -0300

    Delete RestauranteExceptionNoLoCrea.java

--- Restaurante/src/Restaurante2/RestauranteExceptionNoLoCrea.java
+++
@@ -1,9 +0,0 @@
-package Restaurante2;
-
-public class RestauranteExceptionNoLoCrea extends Exception {
-	public RestauranteExceptionNoLoCrea() {
-		super("El Restaurante no pudo ser creado");
-	}
-
-
-}
